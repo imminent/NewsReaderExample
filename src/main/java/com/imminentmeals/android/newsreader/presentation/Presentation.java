ec6844f4426ca3c67b65eecc75b9a404fe866eec
Dandre Allison <dandre.allison@gmail.com>
2013-04-01 10:30:30 -0700

    Changes @Protocol to @HasProtocol

--- src/main/java/com/imminentmeals/android/newsreader/presentation/Presentation.java
+++ src/main/java/com/imminentmeals/android/newsreader/presentation/Presentation.java
@@ -1,6 +1,5 @@
 package com.imminentmeals.android.newsreader.presentation;
 
-import com.imminentmeals.android.newsreader.controller.Controller;
 
 /**
  *
@@ -9,12 +8,4 @@ import com.imminentmeals.android.newsreader.controller.Controller;
 public interface Presentation {
 	String EXTRA = "com.imminentmeals.android.newsreader.presentation.Extra.";
 	String KEY = "com.imminentmeals.android.newsreader.presentation.Key.";
-		
-	@SuppressWarnings("serial")
-	class IncompatibleControllerException extends IllegalArgumentException {
-		
-		public IncompatibleControllerException(Controller controller) {
-			super("Attempting to attach imcompatible Controller: " + controller.getClass().getName());
-		}
-	}
 }
